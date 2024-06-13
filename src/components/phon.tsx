0300198c5c40f4559c1f0b98de2c7763cad5505a
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-06-13 19:53:02 +0530

    changedq

--- src/components/phon.tsx
+++ src/components/phon.tsx
@@ -1,6 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import { cn } from "@/lib/utils";
-import { HTMLAttributes } from "react";
-
+import React, { HTMLAttributes } from "react";
 interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
   imgSrc: string;
   dark?: boolean;
@@ -10,7 +10,7 @@ const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
   return (
     <div
       className={cn(
-        "relative pointer-events-none z-50 overflow-hidden",
+        "relative pointer-events-none z-50 overflow-auto",
         className
       )}
       {...props}
@@ -19,18 +19,13 @@ const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
         src={
           dark
             ? "/phone-template-dark-edges.png"
-            : "/phone-template-white-edges.png"
+            : "phone-template-white-edges.png"
         }
-        className="pointer-events-none z-50 select-none"
-        alt="phone image"
+        alt="dark-white"
+        className=" pointer-events-none z-50  select-none"
       />
-
       <div className="absolute -z-10 inset-0">
-        <img
-          className="object-cover min-w-full min-h-full"
-          src={imgSrc}
-          alt="overlaying phone image"
-        />
+        <img src={imgSrc} className=" object-cover" alt="imgSrc" />
       </div>
     </div>
   );
--- src/components/rev.tsx
+++ src/components/rev.tsx
@@ -1,11 +1,9 @@
 "use client";
-
-import { HTMLAttributes, useEffect, useRef, useState } from "react";
+import React, { HTMLAttributes, useEffect, useRef, useState } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { useInView } from "framer-motion";
 import { cn } from "@/lib/utils";
 import Phone from "./Phone";
-
 const PHONES = [
   "/testimonials/1.jpg",
   "/testimonials/2.jpg",
@@ -17,7 +15,6 @@ const PHONES = [
 
 function splitArray<T>(array: Array<T>, numParts: number) {
   const result: Array<Array<T>> = [];
-
   for (let i = 0; i < array.length; i++) {
     const index = i % numParts;
     if (!result[index]) {
@@ -25,7 +22,6 @@ function splitArray<T>(array: Array<T>, numParts: number) {
     }
     result[index].push(array[i]);
   }
-
   return result;
 }
 
@@ -43,32 +39,27 @@ function ReviewColumn({
   const columnRef = useRef<HTMLDivElement | null>(null);
   const [columnHeight, setColumnHeight] = useState(0);
   const duration = `${columnHeight * msPerPixel}ms`;
-
   useEffect(() => {
     if (!columnRef.current) return;
-
     const resizeObserver = new window.ResizeObserver(() => {
       setColumnHeight(columnRef.current?.offsetHeight ?? 0);
     });
-
     resizeObserver.observe(columnRef.current);
-
     return () => {
       resizeObserver.disconnect();
     };
   }, []);
-
   return (
     <div
       ref={columnRef}
-      className={cn("animate-marquee space-y-8 py-4", className)}
+      className={cn("animate-marquee space-y-8 py-4 ", className)}
       style={{ "--marquee-duration": duration } as React.CSSProperties}
     >
       {reviews.concat(reviews).map((imgSrc, reviewIndex) => (
         <Review
           key={reviewIndex}
-          className={reviewClassName?.(reviewIndex % reviews.length)}
           imgSrc={imgSrc}
+          className={reviewClassName?.(reviewIndex % reviews.length)}
         />
       ))}
     </div>
@@ -78,7 +69,6 @@ function ReviewColumn({
 interface ReviewProps extends HTMLAttributes<HTMLDivElement> {
   imgSrc: string;
 }
-
 function Review({ imgSrc, className, ...props }: ReviewProps) {
   const POSSIBLE_ANIMATION_DELAYS = [
     "0s",
@@ -88,20 +78,18 @@ function Review({ imgSrc, className, ...props }: ReviewProps) {
     "0.4s",
     "0.5s",
   ];
-
   const animationDelay =
     POSSIBLE_ANIMATION_DELAYS[
       Math.floor(Math.random() * POSSIBLE_ANIMATION_DELAYS.length)
     ];
-
   return (
     <div
+      {...props}
       className={cn(
-        "animate-fade-in rounded-[2.25rem] bg-white p-6 opacity-0 shadow-xl shadow-slate-900/5",
+        "animate-fade-in rounded-[2.25rem] bg-white p-6 opacity-0 shadow-xl shadow-slate-900/5 ",
         className
       )}
       style={{ animationDelay }}
-      {...props}
     >
       <Phone imgSrc={imgSrc} />
     </div>
@@ -119,7 +107,7 @@ function ReviewGrid() {
   return (
     <div
       ref={containerRef}
-      className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"
+      className=" relative -mx-4  mt-16 grid h-[49rem] max-h-[150vh] grid-col-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 mg:grid-cols-2 lg:grid-cols-3"
     >
       {isInView ? (
         <>
@@ -148,22 +136,22 @@ function ReviewGrid() {
           />
         </>
       ) : null}
-      <div className="pointer-events-none absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-slate-100" />
-      <div className="pointer-events-none absolute inset-x-0 bottom-0 h-32 bg-gradient-to-t from-slate-100" />
+      <div className="pointer-events-none absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-slate-100"></div>
+      <div className="pointer-events-none absolute inset-x-0 bottom-0 h-32 bg-gradient-to-t from-slate-100"></div>
     </div>
   );
 }
 
-export function Reviews() {
+export const Reviews = () => {
   return (
-    <MaxWidthWrapper className="relative max-w-5xl">
+    <MaxWidthWrapper className=" relative max-w-5xl">
       <img
         aria-hidden="true"
-        src="/what-people-are-buying.png"
-        className="absolute select-none hidden xl:block -left-32 top-1/3"
+        src="what-people-are-buying.png"
+        alt="people"
+        className=" absolute select-none hidden xl:block -left-32 top-1/3"
       />
-
       <ReviewGrid />
     </MaxWidthWrapper>
   );
-}
+};
